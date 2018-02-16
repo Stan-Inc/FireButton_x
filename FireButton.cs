b0e9864a98ab63ef30808c550e7ebd2f68914aab
Satan-Inc <36431133+Satan-Inc@users.noreply.github.com>
2018-02-15 21:14:35 -0700

    Add files via upload

---
+++ FireButton.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireButton : MonoBehaviour {
+
+    private int hitRange = 1;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Attack();
+        }	
+	}
+
+    void Attack()
+    {
+        RaycastHit hit;
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 origin = transform.position;
+
+        if(Physics.Raycast(origin, forward, out hit, hitRange))
+        {
+            if(hit.transform.gameObject.tag == "Enemy")
+            {
+                hit.transform.gameObject.SendMessage("Take Damage", 30);
+            }
+        }
+    }
+}
---
+++ attackButton.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class attackButton : MonoBehaviour {
+
+    public float attackDuration = 0.3f;
+    public bool attacking;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            attacking = true;
+        }
+        EnableDamage();
+	}
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag == "Enemy")
+        {
+            if(col.tag == "Enemy")
+            {
+                col.SendMessage("LevaDano", 1, SendMessageOptions.DontRequireReceiver);
+            }
+        }
+        attacking = false;
+    }
+    void EnableDamage()
+    {
+        if (attacking == true) return;
+        attacking = true;
+        StartCoroutine("DisableDamage");
+    }
+    IEnumerator DisableDamage()
+    {
+        yield return new WaitForSeconds(attackDuration);
+        attacking = false;
+    }
+}
